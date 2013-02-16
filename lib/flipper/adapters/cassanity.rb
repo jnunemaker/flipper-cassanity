883488e1b94b8715a628a5de4ede464eea430b48
John Nunemaker <nunemaker@gmail.com>
2013-02-16 09:56:43 -0500

    Doc and refactor. Should be more obvious what is going on.

--- lib/flipper/adapters/cassanity.rb
+++ lib/flipper/adapters/cassanity.rb
@@ -4,39 +4,37 @@ require 'cassanity'
 module Flipper
   module Adapters
     class Cassanity
+      # Private: The key that stores the set of known features.
       FeaturesKey = :flipper_features
 
+      # Public: The name of the adapter.
       attr_reader :name
 
+      # Private: The column family where the data is stored.
+      attr_reader :column_family
+
+      # Public: Initializes a Cassanity flipper adapter.
+      #
+      # column_family - The Cassanity::ColumnFamily that should store the info.
       def initialize(column_family)
         @column_family = column_family
         @name = :cassanity
       end
 
-      # Public
+      # Public: Gets the values for all gates for a given feature.
+      #
+      # Returns a Hash of Flipper::Gate#key => value.
       def get(feature)
         result = {}
-
-        rows = @column_family.select({
-          select: [:field, :value],
-          where: {
-            key: feature.key,
-          },
-        })
+        doc = doc_for(feature)
+        fields = doc.keys
 
         feature.gates.each do |gate|
           result[gate.key] = case gate.data_type
           when :boolean, :integer
-            if gate_row = rows.detect { |row| row['field'] == gate.key.to_s }
-              gate_row['value']
-            end
+            doc[gate.key.to_s]
           when :set
-            regex = /^#{Regexp.escape(gate.key)}\//
-
-            gate_rows = rows.select { |row| row['field'] =~ regex }
-            gate_keys = gate_rows.map { |row| row['field'] }
-            gate_values = gate_keys.map { |key| key.split('/', 2).last }
-            gate_values.to_set
+            fields_to_gate_value(fields, gate)
           else
             unsupported_data_type(gate.data_type)
           end
@@ -45,29 +43,19 @@ module Flipper
         result
       end
 
-      # Public
+      # Public: Enables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being disabled for the gate.
+      #
+      # Returns true.
       def enable(feature, gate, thing)
         case gate.data_type
         when :boolean, :integer
-          @column_family.update({
-            set: {
-              value: thing.value.to_s,
-            },
-            where: {
-              key: feature.key,
-              field: gate.key,
-            },
-          })
+          update feature.key, gate.key, thing.value.to_s
         when :set
-          @column_family.update({
-            set: {
-              value: 1,
-            },
-            where: {
-              key: feature.key,
-              field: "#{gate.key}/#{thing.value}",
-            },
-          })
+          update feature.key, to_field(gate, thing), thing.value.to_s
         else
           unsupported_data_type(gate.data_type)
         end
@@ -75,32 +63,21 @@ module Flipper
         true
       end
 
-      # Public
+      # Public: Disables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being disabled for the gate.
+      #
+      # Returns true.
       def disable(feature, gate, thing)
         case gate.data_type
         when :boolean
-          @column_family.delete({
-            where: {
-              key: feature.key,
-            },
-          })
+          delete feature.key
         when :integer
-          @column_family.update({
-            set: {
-              value: thing.value.to_s,
-            },
-            where: {
-              key: feature.key,
-              field: gate.key,
-            },
-          })
+          update feature.key, gate.key, thing.value.to_s
         when :set
-          @column_family.delete({
-            where: {
-              key: feature.key,
-              field: "#{gate.key}/#{thing.value}"
-            },
-          })
+          delete feature.key, to_field(gate, thing)
         else
           unsupported_data_type(gate.data_type)
         end
@@ -110,28 +87,80 @@ module Flipper
 
       # Public: Adds a feature to the set of known features.
       def add(feature)
-        @column_family.update({
-          set: {
-            value: 1,
-          },
-          where: {
-            key: FeaturesKey,
-            field: feature.name,
-          },
-        })
+        update FeaturesKey, feature.name, 1
         true
       end
 
       # Public: The set of known features.
       def features
-        rows = @column_family.select({
-          select: [:field, :value],
-          where: {key: FeaturesKey.to_s},
-        })
+        rows = select(FeaturesKey)
         rows.map { |row| row['field'] }.to_set
       end
 
       # Private
+      def to_field(gate, thing)
+        "#{gate.key}/#{thing.value}"
+      end
+
+      # Private: Select rows matching key and optionally field.
+      #
+      # Returns an Array of Hashes.
+      def select(key, field = :skip)
+        @column_family.select({
+          select: [:field, :value],
+          where: where(key, field),
+        })
+      end
+
+      # Private: Update key/field combo to value.
+      #
+      # Returns nothing.
+      def update(key, field, value)
+        @column_family.update({
+          set: {value: value},
+          where: {key: key, field: field},
+        })
+      end
+
+      # Private: Delete rows matching key and optionally field as well.
+      #
+      # Returns nothing.
+      def delete(key, field = :skip)
+        @column_family.delete({
+          where: where(key, field),
+        })
+      end
+
+      # Private: Given a key and field it returns appropriate where hash for
+      # querying the column family.
+      #
+      # Returns a Hash to be used as criteria for a query.
+      def where(key, field)
+        where = {key: key}
+        where[:field] = field unless field == :skip
+        where
+      end
+
+      # Private: Gets a hash of fields => values for the given feature.
+      #
+      # Returns a Hash of fields => values.
+      def doc_for(feature)
+        field_value_pairs = select(feature.key).map { |row| row.values }
+        Hash[*field_value_pairs.flatten]
+      end
+
+      # Private: Returns a set for a gate based on an array of fields/values.
+      #
+      # Returns a Set of the values enabled for the gate.
+      def fields_to_gate_value(fields, gate)
+        regex = /^#{Regexp.escape(gate.key)}\//
+        keys = fields.grep(regex)
+        values = keys.map { |key| key.split('/', 2).last }
+        values.to_set
+      end
+
+      # Private: Raises error letting user know that data type is not
+      # supported by this adapter.
       def unsupported_data_type(data_type)
         raise "#{data_type} is not supported by this adapter"
       end
